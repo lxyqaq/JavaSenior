6a3be31c79ea0b925f5e33f7fcd7ea58e817f5e4
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-01-11 22:37:22 +0000

    Java高级开发开始

---
+++ day01_Java多线程/src/javacode/HelloWorld.java
@@ -0,0 +1,25 @@
+package javacode;
+
+/**
+ * @ClassName HelloWorld
+ * @Description TODO
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2021/1/11 22:31
+ * @Version 1.0
+ */
+public class HelloWorld {
+
+    static int num = 10;
+    public static final int NUMBER = 1;
+
+    public static void main(String[] args) {
+        System.out.println(123);
+        System.out.println(123);
+
+        System.out.println("HelloWorld.main");
+        System.out.println("args = [" + args + "]");
+
+        System.out.println("num = " + num);
+    }
+
+}
