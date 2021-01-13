26e96199ffc0dd8f80e156d4f2060dc7d1ced370
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-01-13 09:40:50 +0000

    课堂问题

--- Test/src/Q1Skel.java
+++ Test/src/Q1Skel.java
@@ -1,14 +1,14 @@
-class Display extends Thread {
+class Display implements Runnable {
 
-    public String name;
+    /*public String name;
 
     public Display(String nm) {
         name = nm;
-    }
+    }*/
 
     public void run() {
         for (int i = 0; i < 4; i++) {
-            System.out.println(name + ": " + i);
+            System.out.println(Thread.currentThread().getName() + ": " + i);
             try {
                 Thread.sleep(1000);
             } catch (Exception e) {
@@ -22,9 +22,10 @@ class Display extends Thread {
 
 public class Q1Skel {
     public static void main(String[] args) {
-        Display d1 = new Display("First");
-        Display d2 = new Display("Second");
-        d1.start();
-        d2.start();
+        Display d1 = new Display();
+        Thread thread1 = new Thread(d1);
+        Thread thread2 = new Thread(d1);
+        thread1.start();
+        thread2.start();
     }
 }
