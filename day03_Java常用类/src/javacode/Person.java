832751124219cdf739b31693a6ccdb939738cd8d
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-01-17 22:04:26 +0000

    重新认识String

---
+++ day03_Java常用类/src/javacode/Person.java
@@ -0,0 +1,24 @@
+package javacode;
+
+/**
+ * @ClassName Person
+ * @Description TODO
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2021/1/17 21:58
+ * @Version 1.0
+ */
+public class Person {
+
+    public String name;
+    public int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+
+    }
+
+}
--- day03_Java常用类/src/javacode/StringTest.java
+++ day03_Java常用类/src/javacode/StringTest.java
@@ -53,8 +53,41 @@ public class StringTest {
 
     }
 
+    /**
+     * String的实例化方式：
+     * 方式一：通过字面量定义的方式
+     * 方式二：通过new + 构造器的方式
+     * <p>
+     * 面试题：String s = new String("abc");方式创建对象，在内存中创建了几个对象？
+     * 两个:一个是堆空间中new结构，另一个是char[]对应的常量池中的数据："abc"
+     */
     @Test
     public void test2() {
+
+        //通过字面量定义的方式：此时的s1和s2的数据javaEE声明在方法区中的字符串常量池中。
+        String s1 = "javaEE";
+        String s2 = "javaEE";
+        //通过new + 构造器的方式:此时的s3和s4保存的地址值，是数据在堆空间中开辟空间以后对应的地址值。
+        String s3 = new String("javaEE");
+        String s4 = new String("javaEE");
+
+        System.out.println(s1 == s2);//true
+        System.out.println(s1 == s3);//false
+        System.out.println(s1 == s4);//false
+        System.out.println(s3 == s4);//false
+
+        System.out.println("*****************");
+
+        Person p1 = new Person("Tom", 12);
+        Person p2 = new Person("Tom", 12);
+
+        System.out.println(p1.name.equals(p2.name));//true
+        System.out.println(p1.name == p2.name);//true
+
+        p1.name = "Jerry";
+        System.out.println(p2.name);//Tom
+        System.out.println(p1.name.equals(p2.name));//false
+        System.out.println(p1.name == p2.name);//false
 
     }
 
