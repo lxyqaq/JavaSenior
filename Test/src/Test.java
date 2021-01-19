069c5bfe3a369b622bf50e5ebf3d4382e4fa48b7
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-01-19 22:51:17 +0000

    String的一个面试题

---
+++ Test/src/Test.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2021/1/18 15:28
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Please enter a string of characters: ");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        int count = 0;
+    }
+}
