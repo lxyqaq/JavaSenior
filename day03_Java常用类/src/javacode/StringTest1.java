388c02b4f8328ed82d8105cec5b40682f0bc1d31
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-01-20 14:51:48 +0000

    String类与其他结构之间的转换

--- day03_Java常用类/src/javacode/StringTest1.java
+++ day03_Java常用类/src/javacode/StringTest1.java
@@ -13,10 +13,10 @@ public class StringTest1 {
 
     /**
      * 复习：
-     *     String 与基本数据类型、包装类之间的转换。
-     *
-     *     String --> 基本数据类型、包装类：调用包装类的静态方法：parseXxx(str)
-     *     基本数据类型、包装类 --> String:调用String重载的valueOf(xxx)
+     * String 与基本数据类型、包装类之间的转换。
+     * <p>
+     * String --> 基本数据类型、包装类：调用包装类的静态方法：parseXxx(str)
+     * 基本数据类型、包装类 --> String:调用String重载的valueOf(xxx)
      */
     @Test
     public void test1() {
@@ -28,6 +28,28 @@ public class StringTest1 {
         System.out.println(num);
 
         String str2 = String.valueOf(num);
+        System.out.println(str2);
+
+    }
+
+    /**
+     * String 与 char[]之间的转换
+     * <p>
+     * String --> char[]:调用String的toCharArray()
+     * char[] --> String:调用String的构造器
+     */
+    @Test
+    public void test2() {
+
+        String str1 = "abc123";
+
+        char[] charArray = str1.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            System.out.println(charArray[i]);
+        }
+
+        char[] arr = new char[]{'h', 'e', 'l', 'l', 'o'};
+        String str2 = new String(arr);
         System.out.println(str2);
 
     }
