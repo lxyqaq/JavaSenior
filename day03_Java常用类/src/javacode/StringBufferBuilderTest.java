e60bca57be33238e7fdc90c3af0845d7e8bc26d1
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-01-20 15:37:12 +0000

    StringBuffer

---
+++ day03_Java常用类/src/javacode/StringBufferBuilderTest.java
@@ -0,0 +1,19 @@
+package javacode;
+
+import org.junit.Test;
+
+/**
+ * @ClassName StringBufferBuilderTest
+ * @Description TODO
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2021/1/20 15:14
+ * @Version 1.0
+ */
+public class StringBufferBuilderTest {
+
+    @Test
+    public void test1() {
+
+    }
+
+}
