386e0fee6894a074c88888f37565c087a77de3d8
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-01-21 21:51:39 +0000

    FileReaderWriter

--- day09_Java的IO流/src/javacode/FileReaderWriterTest.java
+++ day09_Java的IO流/src/javacode/FileReaderWriterTest.java
@@ -151,4 +151,70 @@ public class FileReaderWriterTest {
 
     }
 
+    @Test
+    public void test4() {
+
+        FileReader fr = null;
+        FileWriter fw = null;
+        try {
+            //1.创建File类的对象，指明读入和写出的文件
+            File srcFile = new File("hello.txt");
+            File destFile = new File("hello2.txt");
+
+            //不能使用字符流来处理图片等字节数据
+//            File srcFile = new File("爱情与友情.jpg");
+//            File destFile = new File("爱情与友情1.jpg");
+
+
+            //2.创建输入流和输出流的对象
+            fr = new FileReader(srcFile);
+            fw = new FileWriter(destFile);
+
+
+            //3.数据的读入和写出操作
+            char[] cbuf = new char[5];
+            int len;//记录每次读入到cbuf数组中的字符的个数
+            while ((len = fr.read(cbuf)) != -1) {
+                //每次写出len个字符
+                fw.write(cbuf, 0, len);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //4.关闭流资源
+            //方式一：
+//            try {
+//                if(fw != null)
+//                    fw.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }finally{
+//                try {
+//                    if(fr != null)
+//                        fr.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+            //方式二：
+            try {
+                if (fw != null)
+                    fw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                if (fr != null)
+                    fr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+
 }
