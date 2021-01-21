d7f9210200d1bf0e1826aa4593511106bf5ee6ac
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-01-21 20:38:35 +0000

    FileReaderWriter

--- day09_Java的IO流/src/javacode/FileReaderWriterTest.java
+++ day09_Java的IO流/src/javacode/FileReaderWriterTest.java
@@ -2,10 +2,7 @@ package javacode;
 
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * @ClassName FileReaderWriterTest
@@ -112,6 +109,46 @@ public class FileReaderWriterTest {
 
             }
         }
+    }
+
+    /**
+     * 从内存中写出数据到硬盘的文件里。
+     * <p>
+     * 说明：
+     * 1. 输出操作，对应的File可以不存在的。并不会报异常
+     * 2.
+     * File对应的硬盘中的文件如果不存在，在输出的过程中，会自动创建此文件。
+     * File对应的硬盘中的文件如果存在：
+     * 如果流使用的构造器是：FileWriter(file,false) / FileWriter(file):对原有文件的覆盖
+     * 如果流使用的构造器是：FileWriter(file,true):不会对原有文件覆盖，而是在原有文件基础上追加内容
+     */
+    @Test
+    public void test3() {
+        FileWriter fw = null;
+
+        try {
+            //1.提供File类的对象，指明写出到文件
+            File file = new File("hello.txt");
+
+            //2.提供FileWriter的对象，用于数据的写出
+            fw = new FileWriter(file, true);
+
+            //3.写出的操作
+            fw.write("I have a dream!");
+            fw.write("You has a dream!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //4.流资源的关闭
+            if (fw != null) {
+                try {
+                    fw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 
 }
