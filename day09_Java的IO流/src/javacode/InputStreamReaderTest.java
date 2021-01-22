3eca09c1e32a8d11139ec7474e01ec1cc1d2a330
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-01-22 22:15:16 +0000

    转化流

--- day09_Java的IO流/src/javacode/InputStreamReaderTest.java
+++ day09_Java的IO流/src/javacode/InputStreamReaderTest.java
@@ -2,9 +2,7 @@ package javacode;
 
 import org.junit.Test;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /**
  * @ClassName InputStreamReaderTest
@@ -41,6 +39,33 @@ public class InputStreamReaderTest {
                 }
             }
         }
+
+    }
+
+    @Test
+    public void test2() throws IOException {
+
+        //1.造文件、造流
+        File file1 = new File("dbcp.txt");
+        File file2 = new File("dbcp_gbk.txt");
+
+        FileInputStream fis = new FileInputStream(file1);
+        FileOutputStream fos = new FileOutputStream(file2);
+
+        InputStreamReader isr = new InputStreamReader(fis, "utf-8");
+        OutputStreamWriter osw = new OutputStreamWriter(fos, "gbk");
+
+        //2.读写过程
+        char[] cbuf = new char[20];
+        int len;
+        while ((len = isr.read(cbuf)) != -1) {
+            osw.write(cbuf, 0, len);
+        }
+
+        //3.关闭资源
+        isr.close();
+        osw.close();
+
 
     }
 
