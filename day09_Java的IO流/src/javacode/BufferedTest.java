5fd985747ac6026ba5f5d8cb507c06b0e1fdd865
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-01-22 21:19:08 +0000

    Buffered缓冲流

--- day09_Java的IO流/src/javacode/BufferedTest.java
+++ day09_Java的IO流/src/javacode/BufferedTest.java
@@ -61,4 +61,60 @@ public class BufferedTest {
 
     }
 
+    @Test
+    public void test2() {
+
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        try {
+            //创建文件和相应的流
+            br = new BufferedReader(new FileReader(new File("dbcp.txt")));
+            bw = new BufferedWriter(new FileWriter(new File("dbcp1.txt")));
+
+            //读写操作
+            //方式一：使用char[]数组
+//            char[] cbuf = new char[1024];
+//            int len;
+//            while((len = br.read(cbuf)) != -1){
+//                bw.write(cbuf,0,len);
+//    //            bw.flush();
+//            }
+
+            //方式二：使用String
+            String data;
+            while ((data = br.readLine()) != null) {
+                //方法一：
+//                bw.write(data + "\n");//data中不包含换行符
+                //方法二：
+                bw.write(data);//data中不包含换行符
+                bw.newLine();//提供换行的操作
+
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //关闭资源
+            if (bw != null) {
+
+                try {
+                    bw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+    }
 }
+
+
