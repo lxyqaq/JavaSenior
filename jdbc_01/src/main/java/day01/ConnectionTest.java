2290fab7950ae21fb2b138f9a146458e295aaebd
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-01-25 23:04:38 +0000

    jdbc

---
+++ jdbc_01/src/main/java/day01/ConnectionTest.java
@@ -0,0 +1,19 @@
+package day01;
+
+import org.junit.Test;
+
+/**
+ * @ClassName ConnectionTest
+ * @Description TODO
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2021/1/25 18:08
+ * @Version 1.0
+ */
+public class ConnectionTest {
+
+    @Test
+    public void test1() {
+
+    }
+
+}
---
+++ jdbc_01/src/main/java/util/JDBCUtils.java
@@ -0,0 +1,93 @@
+package util;
+
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
+/**
+ * @ClassName JDBCUtils
+ * @Description TODO
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2021/1/25 22:50
+ * @Version 1.0
+ */
+public class JDBCUtils {
+
+    /**
+     * @return conn
+     * @throws Exception
+     * @description 获取数据库的连接
+     * @author lxyqaq @email A00279565@student.ait.ie
+     * @date 2021/1/25 23:00
+     */
+    public static Connection getConnection() throws Exception {
+
+        InputStream is = ClassLoader.getSystemClassLoader().getResourceAsStream("jdbc.properties");
+        Properties pros = new Properties();
+        pros.load(is);
+
+        String user = pros.getProperty("user");
+        String password = pros.getProperty("password");
+        String url = pros.getProperty("url");
+        String driverClass = pros.getProperty("driverClass");
+
+        Class.forName(driverClass);
+
+        Connection conn = DriverManager.getConnection(url, user, password);
+
+        return conn;
+
+    }
+
+    /**
+     * @param conn
+     * @param ps
+     * @description 关闭连接和Statement的操作
+     * @author lxyqaq @email A00279565@student.ait.ie
+     * @date 2021/1/25 23:02
+     */
+    public static void closeResource(Connection conn, Statement ps) {
+        try {
+            if (ps != null)
+                ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (conn != null)
+                conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param conn
+     * @param ps
+     * @param rs
+     * @description 关闭资源操作
+     * @author lxyqaq @email A00279565@student.ait.ie
+     * @date 2021/1/25 23:03
+     */
+    public static void closeResource(Connection conn, Statement ps, ResultSet rs) {
+        try {
+            if (ps != null)
+                ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (conn != null)
+                conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (rs != null)
+                rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
