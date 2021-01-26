4d2d41a804532e169c7abf42c15078e658fe3ab1
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-01-26 00:40:26 +0000

    jdbc

--- jdbc_01/src/main/java/day01/CustomerForQuery.java
+++ jdbc_01/src/main/java/day01/CustomerForQuery.java
@@ -4,10 +4,8 @@ import bean.Customer;
 import org.junit.Test;
 import util.JDBCUtils;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.lang.reflect.Field;
+import java.sql.*;
 
 /**
  * @ClassName CustomerForQuery
@@ -51,6 +49,63 @@ public class CustomerForQuery {
         } finally {
             JDBCUtils.closeResource(conn, ps, resultSet);
         }
+
+    }
+
+    /**
+     * @param sql
+     * @param args
+     * @return bean.Customer
+     * @throws
+     * @throws Exception
+     * @description
+     * @author lxyqaq @email A00279565@student.ait.ie
+     * @date 2021/1/26 00:39
+     */
+    public Customer queryForCustomers(String sql, Object... args) {
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            conn = JDBCUtils.getConnection();
+
+            ps = conn.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+
+            rs = ps.executeQuery();
+            //获取结果集的元数据 :ResultSetMetaData
+            ResultSetMetaData rsmd = rs.getMetaData();
+            //通过ResultSetMetaData获取结果集中的列数
+            int columnCount = rsmd.getColumnCount();
+
+            if (rs.next()) {
+                Customer cust = new Customer();
+                //处理结果集一行数据中的每一个列
+                for (int i = 0; i < columnCount; i++) {
+                    //获取列值
+                    Object columValue = rs.getObject(i + 1);
+
+                    //获取每个列的列名
+//					String columnName = rsmd.getColumnName(i + 1);
+                    String columnLabel = rsmd.getColumnLabel(i + 1);
+
+                    //给cust对象指定的columnName属性，赋值为columValue：通过反射
+                    Field field = Customer.class.getDeclaredField(columnLabel);
+                    field.setAccessible(true);
+                    field.set(cust, columValue);
+                }
+                return cust;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, ps, rs);
+        }
+
+        return null;
 
     }
 
