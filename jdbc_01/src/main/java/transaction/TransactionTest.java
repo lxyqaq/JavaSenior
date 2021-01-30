85a48caacc7a5b692a0f6b4b62810bb883cef687
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-01-30 19:47:31 +0000

    数据库事务

---
+++ jdbc_01/src/main/java/transaction/TransactionTest.java
@@ -0,0 +1,16 @@
+package transaction;
+
+/**
+ * @ClassName TransactionTest
+ * @Description TODO
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2021/1/27 20:17
+ * @Version 1.0
+ */
+public class TransactionTest {
+
+    public static void main(String[] args) {
+
+    }
+
+}
