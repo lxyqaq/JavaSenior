c25a332694e7a9bcacc0f61ed818ad21f1c2ef2c
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-02-01 23:51:03 +0000

    注解

---
+++ day04_Java枚举类/src/javacode1/AnnotationTest.java
@@ -0,0 +1,18 @@
+package javacode1;
+
+/**
+ * @ClassName AnnotationTest
+ * @Description TODO
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2021/2/1 21:54
+ * @Version 1.0
+ */
+public class AnnotationTest {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
