e5a062b9fc91e7f358fe5fdc54e21acc7389ba3b
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-02-01 00:44:26 +0000

    数据库事务

---
+++ jdbc_01/src/main/java/bean/User.java
@@ -0,0 +1,54 @@
+package bean;
+
+/**
+ * @ClassName User
+ * @Description TODO
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2021/2/1 00:43
+ * @Version 1.0
+ */
+public class User {
+    private String user;
+    private String password;
+    private int balance;
+
+    public User() {
+        super();
+    }
+
+    public User(String user, String password, int balance) {
+        super();
+        this.user = user;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "User [user=" + user + ", password=" + password + ", balance=" + balance + "]";
+    }
+}
