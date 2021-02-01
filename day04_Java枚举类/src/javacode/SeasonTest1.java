fef8562095d270f1e2120a0ae4048473c6b7b5c1
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-02-01 18:07:26 +0000

    枚举类

---
+++ day04_Java枚举类/src/javacode/SeasonTest1.java
@@ -0,0 +1,97 @@
+package javacode;
+
+/**
+ * @ClassName SeasonTest1
+ * @Description TODO
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2021/2/1 16:44
+ * @Version 1.0
+ */
+
+/**
+ * 使用enum关键字定义枚举类
+ * 说明：定义的枚举类默认继承于java.lang.Enum类
+ */
+public class SeasonTest1 {
+
+    public static void main(String[] args) {
+        Season1 summer = Season1.SUMMER;
+        System.out.println(summer.toString());
+
+        System.out.println("*****************");
+
+        Season1[] values = Season1.values();
+        for (int i = 0; i < values.length; i++) {
+            System.out.println(values[i]);
+            values[i].show();
+        }
+
+        System.out.println("******************");
+
+        Thread.State[] values1 = Thread.State.values();
+        for (int i = 0; i < values1.length; i++) {
+            System.out.println(values1[i]);
+        }
+
+        System.out.println("******************");
+
+        Season1 winter = Season1.valueOf("WINTER");
+        System.out.println(winter);
+        winter.show();
+
+    }
+
+}
+
+interface Info {
+    void show();
+}
+
+enum Season1 implements Info {
+
+    //1.提供当前枚举类的对象，多个对象之间用","隔开，末尾对象";"结束
+    SPRING("春天", "春暖花开") {
+        @Override
+        public void show() {
+            System.out.println("春天在哪里？");
+        }
+    },
+    SUMMER("夏天", "夏日炎炎") {
+        @Override
+        public void show() {
+            System.out.println("宁夏");
+        }
+    },
+    AUTUMN("秋天", "秋高气爽") {
+        @Override
+        public void show() {
+            System.out.println("秋天不回来");
+        }
+    },
+    WINTER("冬天", "冰天雪地") {
+        @Override
+        public void show() {
+            System.out.println("大约在冬季");
+        }
+    };
+
+    //2.声明Season对象的属性:private final修饰
+    private final String seasonName;
+    private final String seasonDesc;
+
+    //3.私有化类的构造器,并给对象属性赋值
+    private Season1(String seasonName, String seasonDesc) {
+        this.seasonName = seasonName;
+        this.seasonDesc = seasonDesc;
+    }
+
+    //4.其他诉求1：获取枚举类对象的属性
+    public String getSeasonName() {
+        return seasonName;
+    }
+
+    public String getSeasonDesc() {
+        return seasonDesc;
+    }
+
+}
