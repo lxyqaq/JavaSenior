1916af12c57ceba6d112fb7e2a20253a7ae4e1c2
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-02-06 20:22:55 +0000

    泛型

---
+++ day06_Java泛型/src/javacode/GenericTest.java
@@ -0,0 +1,19 @@
+package javacode;
+
+import org.junit.Test;
+
+/**
+ * @ClassName GenericTest
+ * @Description TODO
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2021/2/6 20:21
+ * @Version 1.0
+ */
+public class GenericTest {
+
+    @Test
+    public void test1() {
+
+    }
+
+}
