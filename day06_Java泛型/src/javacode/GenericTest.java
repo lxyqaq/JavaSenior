ee108b6b37451ba943c72448f137c82cf6f295f8
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-02-07 16:52:16 +0000

    泛型

--- day06_Java泛型/src/javacode/GenericTest.java
+++ day06_Java泛型/src/javacode/GenericTest.java
@@ -2,6 +2,8 @@ package javacode;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 /**
  * @ClassName GenericTest
  * @Description TODO
@@ -28,9 +30,33 @@ import org.junit.Test;
  */
 public class GenericTest {
 
+    //在集合中没使用泛型的情况
     @Test
     public void test1() {
 
+        ArrayList list = new ArrayList();
+        //需求：存放学生的成绩
+        list.add(78);
+        list.add(76);
+        list.add(89);
+        list.add(88);
+        //问题一：类型不安全
+//        list.add("Tom");
+
+        for(Object score : list) {
+            //问题二：强转时，可能出现ClassCastException
+            int stuScore = (Integer) score;
+
+            System.out.println(stuScore);
+
+        }
+
     }
+
+    @Test
+    public void test2() {
+
+    }
+
 
 }
